2c4a5728315902e1706351009b2245ada3f6fb8e
Prerak Gada <prerakgada07@gmail.com>
2023-12-27 18:39:59 +0530

    Added DeliveryMan Model

---
+++ lib/logic/models/delivery_man_model.dart
@@ -0,0 +1,159 @@
+class DeliveryManModel {
+  String id;
+  String name;
+  String phone;
+  String email;
+  String address;
+  String city;
+  String state;
+  String country;
+  String zipCode;
+  double addressLatitude;
+  double addressLongitude;
+  String status;
+  double rating;
+  String imageUrl;
+  String vehicleType;
+  String vehicleNumber;
+  double currentLatitude;
+  double currentLongitude;
+
+  DeliveryManModel({
+    required this.id,
+    required this.name,
+    required this.phone,
+    required this.email,
+    required this.address,
+    required this.city,
+    required this.state,
+    required this.country,
+    required this.zipCode,
+    required this.addressLatitude,
+    required this.addressLongitude,
+    required this.status,
+    required this.rating,
+    required this.imageUrl,
+    required this.vehicleType,
+    required this.vehicleNumber,
+    required this.currentLatitude,
+    required this.currentLongitude,
+  });
+
+  factory DeliveryManModel.fromJson(Map<String, dynamic> json) {
+    return DeliveryManModel(
+      id: json['id'],
+      name: json['name'],
+      phone: json['phone'],
+      email: json['email'],
+      address: json['address'],
+      city: json['city'],
+      state: json['state'],
+      country: json['country'],
+      zipCode: json['zipCode'],
+      addressLatitude: json['addressLatitude'],
+      addressLongitude: json['addressLongitude'],
+      status: json['status'],
+      rating: json['rating'],
+      imageUrl: json['imageUrl'],
+      vehicleType: json['vehicleType'],
+      vehicleNumber: json['vehicleNumber'],
+      currentLatitude: json['currentLatitude'],
+      currentLongitude: json['currentLongitude'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'phone': phone,
+      'email': email,
+      'address': address,
+      'city': city,
+      'state': state,
+      'country': country,
+      'zipCode': zipCode,
+      'addressLatitude': addressLatitude,
+      'addressLongitude': addressLongitude,
+      'status': status,
+      'rating': rating,
+      'imageUrl': imageUrl,
+      'vehicleType': vehicleType,
+      'vehicleNumber': vehicleNumber,
+      'currentLatitude': currentLatitude,
+      'currentLongitude': currentLongitude,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'DeliveryManModel(id: $id, name: $name, phone: $phone, email: $email, address: $address, city: $city, state: $state, country: $country, zipCode: $zipCode, addressLatitude: $addressLatitude, addressLongitude: $addressLongitude, status: $status, rating: $rating, imageUrl: $imageUrl, vehicleType: $vehicleType, vehicleNumber: $vehicleNumber, currentLatitude: $currentLatitude, currentLongitude: $currentLongitude)';
+  }
+
+  DeliveryManModel copyWith({
+    String? id,
+    String? name,
+    String? phone,
+    String? email,
+    String? address,
+    String? city,
+    String? state,
+    String? country,
+    String? zipCode,
+    double? addressLatitude,
+    double? addressLongitude,
+    String? status,
+    double? rating,
+    String? imageUrl,
+    String? vehicleType,
+    String? vehicleNumber,
+    double? currentLatitude,
+    double? currentLongitude,
+  }) {
+    return DeliveryManModel(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      phone: phone ?? this.phone,
+      email: email ?? this.email,
+      address: address ?? this.address,
+      city: city ?? this.city,
+      state: state ?? this.state,
+      country: country ?? this.country,
+      zipCode: zipCode ?? this.zipCode,
+      addressLatitude: addressLatitude ?? this.addressLatitude,
+      addressLongitude: addressLongitude ?? this.addressLongitude,
+      status: status ?? this.status,
+      rating: rating ?? this.rating,
+      imageUrl: imageUrl ?? this.imageUrl,
+      vehicleType: vehicleType ?? this.vehicleType,
+      vehicleNumber: vehicleNumber ?? this.vehicleNumber,
+      currentLatitude: currentLatitude ?? this.currentLatitude,
+      currentLongitude: currentLongitude ?? this.currentLongitude,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is DeliveryManModel &&
+        other.id == id &&
+        other.name == name &&
+        other.phone == phone &&
+        other.email == email &&
+        other.address == address &&
+        other.city == city &&
+        other.state == state &&
+        other.country == country &&
+        other.zipCode == zipCode &&
+        other.addressLatitude == addressLatitude &&
+        other.addressLongitude == addressLongitude &&
+        other.status == status &&
+        other.rating == rating &&
+        other.imageUrl == imageUrl &&
+        other.vehicleType == vehicleType &&
+        other.vehicleNumber == vehicleNumber &&
+        other.currentLatitude == currentLatitude &&
+        other.currentLongitude == currentLongitude;
+  }
+}
