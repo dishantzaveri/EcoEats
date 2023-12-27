2674b59f652c83c2ad81160fb214f50a1b975c92
dishantzaveri <zaveridishant@gmail.com>
2023-12-28 01:46:01 +0530

    alan

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import Layout from "./components/Layout/Layout";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
 function App() {
+ const ALAN_API ="86e893def59fa09ea3ab18c2643b5c9e2e956eca572e1d8b807a3e2338fdd0dc/stage";
+  useEffect(() => {
+    alanBtn({
+      key: ALAN_API,
+      onCommand: (commandData) => {
+        if (commandData.command === "go:back") {
+          // Call the client code that will react to the received command
+        }
+      },
+    });
+  }, []);
   return <Layout />;
 }
 
