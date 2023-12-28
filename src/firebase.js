6fcd002ca47645984a7426dc6bfcedeb58228f9d
Vividha <vividha1109@gmail.com>
2023-12-28 07:11:13 +0530

    new

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "@firebase/firestore"
+import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDA4lRhWya0vj422cRQyoTeROkWbuUA9o0",
@@ -8,8 +8,10 @@ const firebaseConfig = {
   storageBucket: "tourmate-b25c4.appspot.com",
   messagingSenderId: "721687862384",
   appId: "1:721687862384:web:3a6cdc2f104a3476cda8b5",
-  measurementId: "G-1ZC5PV5R3Q"
+  measurementId: "G-1ZC5PV5R3Q",
 };
 
 const app = initializeApp(firebaseConfig);
-export const firestore = getFirestore(app)
+const db = getFirestore(app);
+export { db };
+export const firestore = getFirestore(app);
