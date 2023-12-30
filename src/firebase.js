2e9508958b800952cb9de402564f757ad90fcb8e
Prerak Gada <prerakgada07@gmail.com>
2023-12-30 16:09:01 +0530

    Latest

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,21 @@
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "firebase/app";
- import { getFirestore } from "firebase/firestore";
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
- // Your web app's Firebase configuration
- const firebaseConfig = {
-  apiKey: "AIzaSyDA4lRhWya0vj422cRQyoTeROkWbuUA9o0",
-  authDomain: "tourmate-b25c4.firebaseapp.com",
-  projectId: "tourmate-b25c4",
-  storageBucket: "tourmate-b25c4.appspot.com",
-  messagingSenderId: "721687862384",
-  appId: "1:721687862384:web:3a6cdc2f104a3476cda8b5",
-  measurementId: "G-1ZC5PV5R3Q",
- };
- // Initialize Firebase
- 
- const app = initializeApp(firebaseConfig);
- // Export firestore database
- // It will be imported into your react app whenever it is needed
- export const db = getFirestore(app);
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDA4lRhWya0vj422cRQyoTeROkWbuUA9o0",
+    authDomain: "tourmate-b25c4.firebaseapp.com",
+    projectId: "tourmate-b25c4",
+    storageBucket: "tourmate-b25c4.appspot.com",
+    messagingSenderId: "721687862384",
+    appId: "1:721687862384:web:cb1bed54385ee91dcda8b5",
+    measurementId: "G-ZHW7E5TJFV"
+};
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+// Export firestore database
+// It will be imported into your react app whenever it is needed
+export const db = getFirestore(app);
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import { Col, Container, Row } from "reactstrap";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/common-section/CommonSection";
@@ -13,7 +12,6 @@ const Checkout = () => {
   const [enterName, setEnterName] = useState("");
   const [enterEmail, setEnterEmail] = useState("");
   const [enterNumber, setEnterNumber] = useState("");
-  const [enterCountry, setEnterCountry] = useState("");
   const [enterCity, setEnterCity] = useState("");
   const [postalCode, setPostalCode] = useState("");
 
@@ -21,39 +19,91 @@ const Checkout = () => {
   const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
   const shippingCost = 30;
 
+  let lat;
+  let lng;
+
   const totalAmount = cartTotalAmount + Number(shippingCost);
+
+  const names = [
+    "Mike Nefkens",
+    "Denise Doyle",
+    "Adeel Manzoor",
+    "David Kenzer",
+    "Jason Jameson",
+  ];
+  const emails = [
+    "dummyForPich@here.com",
+    "eatEasy@here.com",
+    "hello@here.com",
+    "test@here.com",
+    "pitch@here.com",
+  ];
+
+  const destinationAddresses = [
+    "Energy Building, IIT Bombay",
+    "SAC, IIT Bombay",
+    "Gulmohor, IIT Bombay",
+    "Convo, IIT Bombay",
+    "Laxmi Next Pure Veg Restaurant, Powai",
+  ];
+
+  const prices = ["2000", "1500", "500", "300", "2500"];
+
+  const quantities = ["1", "2", "3", "1", "1"];
+
+  const { setDefaults, fromAddress } = require("react-geocode");
+
+  setDefaults({
+    key: "AIzaSyAMn8HxTpw1qZDpqOAyNwIE5BoAKVKEFr0",
+    language: "en",
+    region: "es",
+  });
+  fromAddress(
+    destinationAddresses[
+      Math.floor(Math.random() * destinationAddresses.length)
+    ]
+  )
+    .then(async ({ results }) => {
+      ({ lat, lng } = await results[0].geometry.location);
+      console.log(lat, lng);
+    })
+    .catch(console.error);
 
   const addOrder = async (e) => {
     e.preventDefault();
 
     try {
       const docRef = await addDoc(collection(db, "orders"), {
-        name: enterName,
-        email: enterEmail,
-        phone: enterNumber,
-        country: enterCountry,
-        city: enterCity,
-        postalCode: postalCode,
+        description: "Spicy Arrabbiata penne with fresh Italian herbs",
+        destinationAddress:
+          destinationAddresses[
+            Math.floor(Math.random() * destinationAddresses.length)
+          ],
+        sourceAddress:
+          destinationAddresses[
+            Math.floor(Math.random() * destinationAddresses.length)
+          ],
+        sourceLatitude: lat,
+        sourceLongitude: lng,
+        destinationLatitude: 19.1302435,
+        destinationLongitude: 72.9186734,
+        destinationName:
+          destinationAddresses[
+            Math.floor(Math.random() * destinationAddresses.length)
+          ],
+        destinationPhoneNumber: "9090909090",
+        imageUrl: "",
+        name: names[Math.floor(Math.random() * names.length)],
+        price: prices[Math.floor(Math.random() * prices.length)],
+        quantity: quantities[Math.floor(Math.random() * quantities.length)],
+        sourceName: names[Math.floor(Math.random() * names.length)],
+        sourcePhoneNumber: "9090909090",
+        status: "Cooking",
       });
       console.log("Document written with ID: ", docRef.id);
     } catch (e) {
       console.error("Error adding document: ", e);
     }
-  };
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    const userShippingAddress = {
-      name: enterName,
-      email: enterEmail,
-      phone: enterNumber,
-      country: enterCountry,
-      city: enterCity,
-      postalCode: postalCode,
-    };
-
-    shippingInfo.push(userShippingAddress);
-    console.log(shippingInfo);
   };
 
   return (
@@ -64,11 +114,12 @@ const Checkout = () => {
           <Row>
             <Col lg="8" md="6">
               <h6 className="mb-4">Shipping Address</h6>
-              <form className="checkout__form" onSubmit={submitHandler}>
+              <form className="checkout__form">
                 <div className="form__group">
                   <input
                     type="text"
                     placeholder="Enter your name"
+                    value={names[Math.floor(Math.random() * names.length)]}
                     required
                     onChange={(e) => setEnterName(e.target.value)}
                   />
@@ -78,6 +129,8 @@ const Checkout = () => {
                   <input
                     type="email"
                     placeholder="Enter your email"
+                    // Set a default value pre filled
+                    value={emails[Math.floor(Math.random() * emails.length)]}
                     required
                     onChange={(e) => setEnterEmail(e.target.value)}
                   />
@@ -86,6 +139,7 @@ const Checkout = () => {
                   <input
                     type="number"
                     placeholder="Phone number"
+                    value="9090909090"
                     required
                     onChange={(e) => setEnterNumber(e.target.value)}
                   />
@@ -93,15 +147,12 @@ const Checkout = () => {
                 <div className="form__group">
                   <input
                     type="text"
-                    placeholder="Country"
-                    required
-                    onChange={(e) => setEnterCountry(e.target.value)}
-                  />
-                </div>
-                <div className="form__group">
-                  <input
-                    type="text"
-                    placeholder="City"
+                    placeholder="Desitnation Address"
+                    value={
+                      destinationAddresses[
+                        Math.floor(Math.random() * destinationAddresses.length)
+                      ]
+                    }
                     required
                     onChange={(e) => setEnterCity(e.target.value)}
                   />
@@ -110,21 +161,24 @@ const Checkout = () => {
                   <input
                     type="number"
                     placeholder="Postal code"
+                    value="400076"
                     required
                     onChange={(e) => setPostalCode(e.target.value)}
                   />
                 </div>
-                <button className="addTOCart__btn">
+                {/* <button className="addTOCart__btn">
                   <Link to="/location">Payment</Link>
-                </button>
+                </button> */}
                 <button
                   type="submit"
                   className="addTOCart__btn"
                   onClick={addOrder}
-                ></button>
+                >
+                  <Link to="/location">Place Order</Link>
+                </button>
               </form>
             </Col>
-            ``
+
             <Col lg="4" md="6">
               <div className="checkout__bill">
                 <h6 className="d-flex align-items-center justify-content-between mb-3">
